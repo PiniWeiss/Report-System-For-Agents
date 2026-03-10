7fc16fc54ef149b5b6a0850f4f6fe1aa451a5d7e
pini weiss <pini6775@gmail.com>
2026-03-10 17:05:36 +0200

    .

--- frontend/src/pages/admin/MennageAgents.tsx
+++ frontend/src/pages/admin/MennageAgents.tsx
@@ -1,8 +1,17 @@
+import { useNavigate } from "react-router"
+import { useAuthStore } from "../../store/auth.store"
+import { useEffect } from "react"
 
 function MennageAgents() {
+  const { user } = useAuthStore()
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (!user) navigate("/login")
+    if (user?.role === "agent") navigate("/user")
+  }, [user])
   return (
     <div>
-MennageAgents
+      MennageAgents
     </div>
   )
 }
